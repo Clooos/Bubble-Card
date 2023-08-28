926f809c44692f09291e9795327520ef024431cf
Samuel <samuel95_54@hotmail.com>
2023-08-28 18:15:32 -0400

    Small device adjustment

--- src/bubble-card.js
+++ src/bubble-card.js
@@ -239,6 +239,7 @@ class BubbleCard extends HTMLElement {
                     background-color: var(--background-color,var(--secondary-background-color));
                     border-radius: 25px;
                     margin-right: 14px;
+                    flex-shrink: 0;
                 }
                 .header-icon {
                     display: inline-flex;
@@ -255,6 +256,7 @@ class BubbleCard extends HTMLElement {
                     line-height: 0px;
                     z-index: 100;
                     font-size: 20px;
+
                 }
                 #header-container p {
                     display: inline-flex;
@@ -877,6 +879,12 @@ class BubbleCard extends HTMLElement {
                 z-index: 2;
                 font-weight: 600;
             }
+
+            .nameContainer p{
+              overflow: hidden;
+              white-space: nowrap;
+              animation: scrollText 10s linear infinite;
+          }
             
             .feedback-element {
                 position: absolute;
