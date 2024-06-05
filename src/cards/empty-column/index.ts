6c4a67601cf015ef66c251bda05d79ae00e9a00e
Cloos <36499953+Clooos@users.noreply.github.com>
2024-06-05 16:12:55 +0200

    Delete src directory

--- src/cards/empty-column/index.ts
+++
@@ -1,7 +0,0 @@
-import { createStructure } from './create.ts';
-
-export function handleEmptyColumn(context) {
-    if (context.cardType !== "empty-column") {
-        createStructure(context);
-    }
-}
