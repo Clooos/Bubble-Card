5bc753ebd84707d842661bf1d9612766cd0fd3d1
Bruno Sabot <brunosabot@gmail.com>
2025-05-16 21:14:49 +0200

    fix: calendar date management and style

--- src/cards/calendar/changes.js
+++ src/cards/calendar/changes.js
@@ -10,6 +10,26 @@ function dateDiffInMinutes(a, b) {
   return Math.floor((b - a) / MS_PER_MINUTES);
 }
 
+function parseEventDateTime(event) {
+  if (event.date) {
+    const parts = event.date.split('-');
+    const year = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const day = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  }
+
+  return new Date(event.dateTime);
+}
+
+const getEventDateKey = (eventStart) => {
+  const d = parseEventDateTime(eventStart);
+  const year = d.getFullYear();
+  const month = (d.getMonth() + 1).toString().padStart(2, '0');
+  const dayOfMonth = d.getDate().toString().padStart(2, '0');
+  return `${year}-${month}-${dayOfMonth}`;
+};
+
 export async function changeEventList(context) {
   const daysOfEvents = context.config.days ?? 7;
 
@@ -27,20 +47,33 @@ export async function changeEventList(context) {
   const events = await Promise.all(promises);
 
   context.events = events.flat()
-    .sort(
-      (a, b) => new Date(a.start.date ?? a.start.dateTime).getTime() - new Date(b.start.date ?? b.start.dateTime).getTime()
-    )
+    .sort((a, b) => {
+      const dateA = parseEventDateTime(a.start);
+      const dateB = parseEventDateTime(b.start);
+
+      const isAllDayA = a.start.date !== undefined;
+      const isAllDayB = b.start.date !== undefined;
+
+      if (isAllDayA && !isAllDayB) {
+        return -1;
+      }
+      if (!isAllDayA && isAllDayB) {
+        return 1;
+      }
+
+      return dateA.getTime() - dateB.getTime();
+    })
     .slice(0, context.config.limit ?? undefined);
 }
 
 export async function changeEvents(context) {
   const t = setupTranslation(context._hass);
   const eventsGroupedByDay = context.events.reduce((acc, event) => {
-    const day = event.start.date ?? event.start.dateTime.split('T')[0];
-    if (!acc[day]) {
-      acc[day] = [];
+    const dayKey = getEventDateKey(event.start);
+    if (!acc[dayKey]) {
+      acc[dayKey] = [];
     }
-    acc[day].push(event);
+    acc[dayKey].push(event);
     return acc;
   }, {});
 
@@ -57,7 +90,7 @@ export async function changeEvents(context) {
   context.elements.calendarCardContent.innerHTML = '';
 
   Object.keys(eventsGroupedByDay).sort().forEach((day) => {
-    const eventDay = new Date(day);
+    const eventDay = parseEventDateTime({date: day});
     const today = new Date();
     const dayNumber = document.createElement('div');
     const getCurrentLocale = context._hass.locale.language;
@@ -80,11 +113,16 @@ export async function changeEvents(context) {
     dayEvents.classList.add('bubble-day-events');
 
     eventsGroupedByDay[day].forEach((event) => {
+      const isAllDay = event.start.date !== undefined;
+      const now = new Date();
+      const eventStart = parseEventDateTime(event.start);
+      const eventEnd = parseEventDateTime(event.end);
+
       const eventTime = document.createElement('div');
       eventTime.classList.add('bubble-event-time');
-      eventTime.innerHTML = event.start.date ? t("cards.calendar.all_day") : new Date(event.start.dateTime).toLocaleTimeString(getCurrentLocale, { hour: 'numeric', minute: 'numeric' });
-      if (!event.start.date && context.config.show_end === true) {
-        eventTime.innerHTML += ` – ${new Date(event.end.dateTime).toLocaleTimeString(getCurrentLocale, { hour: 'numeric', minute: 'numeric' })}`;
+      eventTime.innerHTML = isAllDay ? t("cards.calendar.all_day") : eventStart.toLocaleTimeString(getCurrentLocale, { hour: 'numeric', minute: 'numeric' });
+      if (!isAllDay && context.config.show_end === true) {
+        eventTime.innerHTML += ` – ${eventEnd.toLocaleTimeString(getCurrentLocale, { hour: 'numeric', minute: 'numeric' })}`;
       }
 
       const eventName = document.createElement('div');
@@ -96,7 +134,9 @@ export async function changeEvents(context) {
       const eventColor = document.createElement('div');
       eventColor.classList.add('bubble-event-color');
       eventColor.style.backgroundColor = event.entity.color
-        ? `var(--${event.entity.color}-color)`
+        ? event.entity.color.startsWith('#')
+          ? event.entity.color
+          : `var(--${event.entity.color}-color)`
         : intToRGB(hashCode(event.entity.entity));
       if (event.entity.color === 'transparent') {
         eventColor.style.display = 'none';
@@ -109,16 +149,13 @@ export async function changeEvents(context) {
       eventLine.appendChild(eventName);
       addActions(eventLine, { ...context.config, entity: event.entity.entity });
 
-      const now = new Date();
-      const start = new Date(event.start.dateTime ?? event.start.date);
-      const end = new Date(event.end.dateTime ?? event.end.date);
-      const activeColor = 'var(--bubble-event-accent-color, var(--bubble-accent-color, var(--bubble-primary-color)))';
+      const activeColor = 'var(--bubble-event-accent-color, var(--bubble-accent-color, var(--bubble-default-color)))';
 
-      if (context.config.show_progress === true && event.start.date && start < now) {
+      if (context.config.show_progress === true && isAllDay && eventStart < now) {
         eventLine.style.setProperty('--bubble-event-background-color', activeColor);
-      } else if (context.config.show_progress === true &&event.start.dateTime && start < now) {
-        const durationDiff = dateDiffInMinutes(start, end);
-        const startDiff = dateDiffInMinutes(start, now);
+      } else if (context.config.show_progress === true && !isAllDay && eventStart < now) {
+        const durationDiff = dateDiffInMinutes(eventStart, eventEnd);
+        const startDiff = dateDiffInMinutes(eventStart, now);
         const percentage = 100 * startDiff / durationDiff;
 
         eventLine.style.setProperty('--bubble-event-background-image', `linear-gradient(to right, ${activeColor} ${percentage}%, transparent ${percentage}%)`);
