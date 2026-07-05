bd66168a6ee98eb4b2d1e4162d4c8bef5b583ec7
Clooos <cloosquentin@gmail.com>
2026-07-05 22:40:42 +0200

    Fixed enlarged cards when combined with custom:grid-layout #2448

--- src/tools/init.js
+++ src/tools/init.js
@@ -3,12 +3,15 @@
 export function initializeContent(context) {
     if (!context.content) {
         let shadow = context.shadowRoot || context.attachShadow({ mode: 'open' });
+        let hostStyle = document.createElement("style");
+        hostStyle.textContent = ":host{display:inline-block;width:100%;vertical-align:top;}";
         let card = document.createElement("ha-card");
         card.style.cssText = "background: none; border: none; box-shadow: none; border-radius: 16px;";
         let content = document.createElement("div");
         content.className = "card-content";
         content.style.padding = "0";
         card.appendChild(content);
+        shadow.appendChild(hostStyle);
         shadow.appendChild(card);
         context.card = card;
         context.content = content;
